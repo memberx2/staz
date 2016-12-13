41c49f856cb187f52c5bb628c08df1241ef248ca
Joanna <asiad706@gmail.com>
2016-12-13 21:15:38 +0100

    CSS update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".people-pictures").slick({
+        infinite: true,
+        slidesToShow: 21,
+        slidesToScroll: 1,
+        rtl: true,
+        arrows: false,
+        //autoplay: true,
+        //autoplaySpeed: 1000
+    });
+});
